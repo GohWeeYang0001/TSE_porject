e3427bb48eb614cb47d6c269a671c7179f2d598c
darren <darrennew3636@gmail.com>
2025-05-21 23:02:54 +0800

    darren

--- patient/booking.php
+++ patient/booking.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
-        
     <title>Sessions</title>
     <style>
         .popup{
@@ -258,7 +257,7 @@
                                                             Session Title: '.$title.'<br>
                                                             Session Scheduled Date: '.$scheduledate.'<br>
                                                             Session Starts : '.$scheduletime.'<br>
-                                                            Channeling fee : <b>LKR.2 000.00</b>
+                                                            Channeling fee : <b>RM50.00</b>
 
                                                         </div>
                                                         <br>
