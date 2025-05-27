953c4f396e3a400cccafd812d790f98e284dd2e9
darren <darrennew3636@gmail.com>
2025-05-27 16:36:53 +0800

    darren

--- SQL_Database_edoc.sql
+++ SQL_Database_edoc.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 19, 2022 at 01:39 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.5
+-- 主机： 127.0.0.1
+-- 生成日期： 2025-05-27 10:35:44
+-- 服务器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,24 +18,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `edoc`
+-- 数据库： `edoc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- 表的结构 `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `aemail` varchar(255) NOT NULL,
-  `apassword` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`aemail`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `apassword` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `admin`
+-- 转存表中的数据 `admin`
 --
 
 INSERT INTO `admin` (`aemail`, `apassword`) VALUES
@@ -45,128 +42,126 @@ INSERT INTO `admin` (`aemail`, `apassword`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointment`
+-- 表的结构 `appointment`
 --
 
-DROP TABLE IF EXISTS `appointment`;
-CREATE TABLE IF NOT EXISTS `appointment` (
-  `appoid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `appointment` (
+  `appoid` int(11) NOT NULL,
   `pid` int(10) DEFAULT NULL,
   `apponum` int(3) DEFAULT NULL,
   `scheduleid` int(10) DEFAULT NULL,
   `appodate` date DEFAULT NULL,
-  PRIMARY KEY (`appoid`),
-  KEY `pid` (`pid`),
-  KEY `scheduleid` (`scheduleid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `email_sent` tinyint(1) DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `appointment`
+-- 转存表中的数据 `appointment`
 --
 
-INSERT INTO `appointment` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`) VALUES
-(1, 1, 1, 1, '2022-06-03');
+INSERT INTO `appointment` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`, `email_sent`) VALUES
+(14, 1, 1, 10, '2025-05-19', 0),
+(26, 1, 2, 16, '2025-05-24', 0),
+(25, 1, 1, 16, '2025-05-24', 0),
+(36, 3, 1, 22, '2025-05-27', 1),
+(37, 3, 1, 20, '2025-05-27', 1),
+(39, 3, 1, 23, '2025-05-27', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `doctor`
+-- 表的结构 `doctor`
 --
 
-DROP TABLE IF EXISTS `doctor`;
-CREATE TABLE IF NOT EXISTS `doctor` (
-  `docid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `doctor` (
+  `docid` int(11) NOT NULL,
   `docemail` varchar(255) DEFAULT NULL,
   `docname` varchar(255) DEFAULT NULL,
   `docpassword` varchar(255) DEFAULT NULL,
   `docnic` varchar(15) DEFAULT NULL,
   `doctel` varchar(15) DEFAULT NULL,
-  `specialties` int(2) DEFAULT NULL,
-  PRIMARY KEY (`docid`),
-  KEY `specialties` (`specialties`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `specialties` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `doctor`
+-- 转存表中的数据 `doctor`
 --
 
 INSERT INTO `doctor` (`docid`, `docemail`, `docname`, `docpassword`, `docnic`, `doctel`, `specialties`) VALUES
-(1, 'doctor@edoc.com', 'Test Doctor', '123', '000000000', '0110000000', 1);
+(5, 'darren@edoc.com', 'Dr Darren', '123', '1222222222', '0178765432', 20),
+(2, 'jiunendarren@edoc.com', 'Dr Tan', '123', '0123456789', '0177766677', 1),
+(3, 'lim@edoc.com', 'Dr Lim', '123', '0301112121212', '0129876543', 18),
+(4, 'Tay@edoc.com', 'Dr Tay', '123', '121212121212', '0177766677', 19);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `patient`
+-- 表的结构 `patient`
 --
 
-DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
-  `pid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `patient` (
+  `pid` int(11) NOT NULL,
   `pemail` varchar(255) DEFAULT NULL,
   `pname` varchar(255) DEFAULT NULL,
   `ppassword` varchar(255) DEFAULT NULL,
   `paddress` varchar(255) DEFAULT NULL,
   `pnic` varchar(15) DEFAULT NULL,
   `pdob` date DEFAULT NULL,
-  `ptel` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`pid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `ptel` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `patient`
+-- 转存表中的数据 `patient`
 --
 
 INSERT INTO `patient` (`pid`, `pemail`, `pname`, `ppassword`, `paddress`, `pnic`, `pdob`, `ptel`) VALUES
-(1, 'patient@edoc.com', 'Test Patient', '123', 'Sri Lanka', '0000000000', '2000-01-01', '0120000000'),
-(2, 'emhashenudara@gmail.com', 'Hashen Udara', '123', 'Sri Lanka', '0110000000', '2022-06-03', '0700000000');
+(2, 'emhashenudara@gmail.com', 'Hashen Udara', '123', 'Sri Lanka', '0110000000', '2022-06-03', '0700000000'),
+(3, 'darrennew3636@gmail.com', 'Loh David', '1234', '26, Jalan Damai 2/4, Taman Damai 2 83000 Batu Pahat, Johor', '030118011125', '2012-02-27', '0177765522');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `schedule`
+-- 表的结构 `schedule`
 --
 
-DROP TABLE IF EXISTS `schedule`;
-CREATE TABLE IF NOT EXISTS `schedule` (
-  `scheduleid` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `schedule` (
+  `scheduleid` int(11) NOT NULL,
   `docid` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `scheduledate` date DEFAULT NULL,
   `scheduletime` time DEFAULT NULL,
-  `nop` int(4) DEFAULT NULL,
-  PRIMARY KEY (`scheduleid`),
-  KEY `docid` (`docid`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `nop` int(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `schedule`
+-- 转存表中的数据 `schedule`
 --
 
 INSERT INTO `schedule` (`scheduleid`, `docid`, `title`, `scheduledate`, `scheduletime`, `nop`) VALUES
-(1, '1', 'Test Session', '2050-01-01', '18:00:00', 50),
-(2, '1', '1', '2022-06-10', '20:36:00', 1),
-(3, '1', '12', '2022-06-10', '20:33:00', 1),
-(4, '1', '1', '2022-06-10', '12:32:00', 1),
-(5, '1', '1', '2022-06-10', '20:35:00', 1),
-(6, '1', '12', '2022-06-10', '20:35:00', 1),
-(7, '1', '1', '2022-06-24', '20:36:00', 1),
-(8, '1', '12', '2022-06-10', '13:33:00', 1);
+(15, '4', '4pm', '2025-05-23', '16:00:00', 8),
+(14, '2', '2pm', '2025-05-23', '14:00:00', 8),
+(13, '3', '12pm', '2025-05-23', '12:00:00', 6),
+(12, '5', '10am Appointment', '2025-05-23', '10:00:00', 6),
+(16, '3', '10am appointment', '2025-05-25', '10:00:00', 8),
+(17, '2', '12pm appointment', '2025-05-24', '12:00:00', 9),
+(19, '5', 'test', '2025-05-24', '23:30:00', 2),
+(20, '2', '5.41pm', '2025-05-27', '17:41:00', 0),
+(21, '5', '6pm', '2025-05-28', '18:00:00', 3),
+(22, '4', '6pm', '2025-05-27', '18:00:00', 0),
+(23, '5', '12', '2025-05-27', '18:07:00', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `specialties`
+-- 表的结构 `specialties`
 --
 
-DROP TABLE IF EXISTS `specialties`;
-CREATE TABLE IF NOT EXISTS `specialties` (
+CREATE TABLE `specialties` (
   `id` int(2) NOT NULL,
-  `sname` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `sname` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `specialties`
+-- 转存表中的数据 `specialties`
 --
 
 INSERT INTO `specialties` (`id`, `sname`) VALUES
@@ -230,25 +225,105 @@ INSERT INTO `specialties` (`id`, `sname`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `webuser`
+-- 表的结构 `webuser`
 --
 
-DROP TABLE IF EXISTS `webuser`;
-CREATE TABLE IF NOT EXISTS `webuser` (
+CREATE TABLE `webuser` (
   `email` varchar(255) NOT NULL,
-  `usertype` char(1) DEFAULT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `usertype` char(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `webuser`
+-- 转存表中的数据 `webuser`
 --
 
 INSERT INTO `webuser` (`email`, `usertype`) VALUES
 ('admin@edoc.com', 'a'),
-('doctor@edoc.com', 'd'),
+('darren@edoc.com', 'd'),
 ('patient@edoc.com', 'p'),
-('emhashenudara@gmail.com', 'p');
+('emhashenudara@gmail.com', 'p'),
+('jiunendarren@edoc.com', 'd'),
+('lim@edoc.com', 'd'),
+('Tay@edoc.com', 'd'),
+('darrennew3636@gmail.com', 'p');
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aemail`);
+
+--
+-- 表的索引 `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appoid`),
+  ADD KEY `pid` (`pid`),
+  ADD KEY `scheduleid` (`scheduleid`);
+
+--
+-- 表的索引 `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`docid`),
+  ADD KEY `specialties` (`specialties`);
+
+--
+-- 表的索引 `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- 表的索引 `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`scheduleid`),
+  ADD KEY `docid` (`docid`);
+
+--
+-- 表的索引 `specialties`
+--
+ALTER TABLE `specialties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 表的索引 `webuser`
+--
+ALTER TABLE `webuser`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- 使用表AUTO_INCREMENT `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用表AUTO_INCREMENT `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- 使用表AUTO_INCREMENT `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `scheduleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
